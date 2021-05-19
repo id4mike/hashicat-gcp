3d136421a0e76a4c597a6657659b23b1f92e89e2
Mike Intrator <mike.intrator@gmail.com>
2021-05-19 17:36:03 +0000

    fixed typo vpc module again

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,10 @@ version = "2.5.0"
 network_name = "gaurav-network" 
 project_id = var.project 
 subnets = [ 
-    { subnet_name = "gaurav-subnet" 
+    { 
+        subnet_name = "gaurav-subnet" 
     subnet_ip = "10.100.10.0/24" 
-    subnet_region = var.region } 
-    ] }
+    subnet_region = var.region
+     } 
+    ] 
+    }
