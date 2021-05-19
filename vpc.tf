bc2761eaddeaecd05c2dc35fe59188033b5abdc9
Mike Intrator <mike.intrator@gmail.com>
2021-05-19 17:34:11 +0000

    fixed typo vpc module again

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,10 @@
-module "network" { source = "app.terraform.io/ricardo-gcp-training/network/google" 
+module "network" { 
+    source = "app.terraform.io/ricardo-gcp-training/network/google" 
 version = "2.5.0" 
 network_name = "gaurav-network" 
 project_id = var.project 
-subnets = [ { subnet_name = "gaurav-subnet" subnet_ip = "10.100.10.0/24" subnet_region = var.region } ] }
+subnets = [ 
+    { subnet_name = "gaurav-subnet" 
+    subnet_ip = "10.100.10.0/24" 
+    subnet_region = var.region } 
+    ] }
